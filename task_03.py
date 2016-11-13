975b28cbb0d45bdf9486a61d0ec67c0ca639f644
Iuliia Ambrokh <iuliia.ambrokh@spsmail.cuny.edu>
2016-11-13 02:27:28 +0000

    Task_03

--- task_03.py
+++ task_03.py
@@ -7,25 +7,37 @@ import time
 
 
 class CustomLogger(object):
+    """Class for logs."""
 
     def __init__(self, logfilename):
+        """Constructor for CustomLogger class."""
         self.logfilename = logfilename
         self.msgs = []
 
     def log(self, msg, timestamp=None):
+        """Function to track logs."""
         if timestamp is None:
             timestamp = time.time()
         self.msgs.append((timestamp, msg))
 
     def flush(self):
+        """Function to open files and record logs."""
         handled = []
-
-        fhandler = open(self.logfilename, 'a')
+        try:
+            fhandler = open(self.logfilename, 'a')
+        except self.log('Cannot open file'):
+            raise
+        except self.log(IOError):
+            pass
         for index, entry in enumerate(self.msgs):
-            fhandler.write(str(entry) + '\n')
-            handled.append(index)
+            try:
+                fhandler.write(str(entry) + '\n')
+                handled.append(index)
+            except self.log(IOError):
+                raise IOError
+            finally:
+                fhandler.close()
 
-        fhandler.close()
-
-        for index in handled[::-1]:
-            del self.msgs[index]
+            for index in handled[::-1]:
+                if self.msgs[index] in handled.append(index):
+                    del self.msgs[index]
