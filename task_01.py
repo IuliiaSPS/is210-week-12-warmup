3aae02bf38c94e2134f2e0401cffb009249de860
Iuliia Ambrokh <iuliia.ambrokh@spsmail.cuny.edu>
2016-11-13 01:34:21 +0000

    Task_01

--- task_01.py
+++ task_01.py
@@ -4,4 +4,10 @@
 
 
 def simple_lookup(var1, var2):
+    """Function to check if key/index is valid."""
+    try:
+        var1[var2]
+    except LookupError:
+        print 'Warning: Your index/key doesn\'t exist'
+        return var1
     return var1[var2]
